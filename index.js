12476863ad83cd6a7604e5ad248d07221ff93da2
Tam T Dang <teedang19@gmail.com>
2014-06-25 22:02:36 -0500

    complete

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
   console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function() {
